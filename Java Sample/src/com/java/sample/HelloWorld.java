ed149622d44babb61c3288fe99baeb9efc99ed61
rituraj <rituraj@rituraj-Lenovo-3000-G530>
2016-08-26 22:35:07 +0530

    checkin

---
+++ Java Sample/src/com/java/sample/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.java.sample;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello wWorld...");
+	}
+
+}
